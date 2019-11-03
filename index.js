411bef0cffe57ea6ed79f59f6c367b9fd1c14e99
George Arnold <george.arnold.88@gmail.com>
2019-11-03 16:39:34 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 
 // add the information in the input to the list by hitting return
 // or clicking the button
-$("input").keypress( function(event) {
-	if (event.which === 13) {
-        //prevents the form from submitting/ reloading
-        event.preventDefault();
-        //takes text typed into field and appends it
+$("form").submit( function (event)  {
+    //prevents default reload and submit to server
+    event.preventDefault();
+    //takes text typed into input field
+    let listItem = $("input").val();
+    //appends item in "input" to <ul class= shopping-list"> with appropriate structure
+    $(".shopping-list").append('<li>\
+        <span class="shopping-item">' + listItem + '</span>\
+        <div class="shopping-item-controls">\
+          <button class="shopping-item-toggle">\
+            <span class="button-label">check</span>\
+          </button>\
+          <button class="shopping-item-delete">\
+            <span class="button-label">delete</span>\
+          </button>\
+        </div>\
+      </li>');
 
-
-		let listItem = $(this).val();
-        $(".shopping-list").append('<li>\
-            <span class="shopping-item">' + listItem + '</span>\
-            <div class="shopping-item-controls">\
-              <button class="shopping-item-toggle">\
-                <span class="button-label">check</span>\
-              </button>\
-              <button class="shopping-item-delete">\
-                <span class="button-label">delete</span>\
-              </button>\
-            </div>\
-          </li>');
-    
-        //clears value of input
-        $("input").val("");
-       
-	}
+    //clears value of input
+    $("input").val("");
+   
 });
 
-//check or uncheck (toggle class) by clicking the check button
-
-
-
+//check or uncheck <span class= "shopping-item"> (toggle class) by clicking the check button
+$("ul.shopping-list").on("click",".shopping-item-toggle",function() {
+    $(this).parent().parent().find(".shopping-item").toggleClass("shopping-item__checked");
+});
 
 // permenantly remove items from the list
+$("ul.shopping-list").on("click",".shopping-item-delete",function() {
+    $(this).parent().parent().remove();
+});
